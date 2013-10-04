d0ccf6eef0d18be58014c8fc195c556f4a26b780
CJ <lim@chernjie.com>
2013-10-04 20:10:16 +0800

    Fixed javascript problems;

--- tracer.php
+++ tracer.php
@@ -1,16 +1,25 @@
 <?php
 
+/**
+ * Smartass Code tracer to help with code debuggings
+ * 
+ * @author chernjie
+ * @example: call Tracer::init() at the beginning of script execution
+ * @example: call Tracer::out() at the end of script execution
+ * @example: call Tracer::add($name, $data) in between scripts
+ */
 class Tracer
 {
 	public static $tracer = array();
-	const CODE_COVERAGE = 'CODE_COVERAGE';
+	const CODE_COVERAGE = 'Code Coverage';
+	const CACHE_DIR     = './';
 
 	public static function init()
 	{
 		if (array_key_exists('tracefile', $_GET)) exit( highlight_file($_GET['tracefile'], true) );
 		if (array_key_exists('mirror', $_GET))
 		{
-			$cache = '.' . DIRECTORY_SEPARATOR . md5($_GET['mirror']) . '.cache';
+			$cache = self::CACHE_DIR . substr(md5($_GET['mirror']), 0, 4) . '.cache';
 			file_exists($cache)
 				|| file_put_contents($cache, file_get_contents($_GET['mirror']));
 			exit( file_get_contents($cache) );
@@ -29,19 +38,35 @@ class Tracer
 	public static function get_code_coverage()
 	{
 		$coverage = xdebug_get_code_coverage();
-		foreach($coverage as $file => $line):
-			if(!file_exists($file)) continue;
+		foreach ($coverage as $file => $line)
+		{
+			if (! file_exists($file)) continue;
+			/**
 			$file_contents = file($file);
 			$code = '';
-			foreach($line as $lineNumber => $int1)
+			foreach ($line as $lineNumber => $int1)
+			{
 				if (isset($file_contents[$lineNumber-1]))
-				$code.= sprintf('%4d: ',$lineNumber).$file_contents[$lineNumber-1];
-			$coverage[$file] = $code;
-		endforeach;
+				{
+					$code.= sprintf('%4d: ',$lineNumber);
+					$code.= $file_contents[$lineNumber-1];
+				}
+			}
+			$coverage[$file] = highlight_string($code, true);
+			/**/
+			$key = preg_replace('/\/vagrant\//', '', $file);
+			unset($coverage[$file]);
+			$coverage[$key] = $file;
+		}
 		if (array_key_exists('cc_sort', $_GET)) ksort($coverage);
 		return $coverage;
 	}
 
+	/**
+	 * @param stdClass $object
+	 * @param string $property
+	 * @return unknown
+	 */
 	private static function object2array($object, $property=null)
 	{
 		$array = preg_replace('/\w+::__set_state/', '', var_export($object, true));
@@ -49,9 +74,13 @@ class Tracer
 		return is_null($property) ? $array : $array[$property];
 	}
 
+	/**
+	 * @param array $arr
+	 * @param string $path
+	 * @return string
+	 */
 	private static function array2ulli($arr, $path='')
 	{
-		$fn = __METHOD__;
 		$string = '<ul>';
 		if(is_array( $arr))
 			foreach($arr as $k=>$v)
@@ -60,24 +89,21 @@ class Tracer
 				switch(true)
 				{
 					case is_string($v) && file_exists($v):
-						$htmlkey = 'file'.preg_replace('/[^a-z0-9_]/i','',$path.$k);
-						$string.= '<li id="'.$htmlkey.'">'.
-							'<span onclick="$(\'#'.$htmlkey.' div\').toggle().load(\'\',\'tracefile='.urlencode($v).'\');">'.
-								preg_replace( '/\/var\/www\/[^\/]+/i', '', is_numeric($k)?$v:$k.': '.$v ).
-							'</span>'.
-							'<div style="display:none">'.$v.'</div></li>';
+						$string.= sprintf('<li><span class="tracefile">%s</span><div>%s</div></li>', $k, $v);
 						break;
 					case empty($v):
-						$string.= '<li><span>'.$k.': '.var_export($v,true).'</span></li>';
+						$string.= sprintf('<li><span>%s: %s</span></li>', $k, var_export($v, true));
 						break;
 					case is_scalar($v):
-						$string.= '<li><span>'.$k.': '.$v.'</span></li>';
+						$string.= sprintf('<li><span>%s: %s</span></li>', $k, $v);
 						break;
 					case is_object($v):
-						$string.= '<li><span>'.$k.': '.get_class($v).'</span>'. self::array2ulli(object2array($v, 'definition'===$k?'definition':null), $path.'/'.$k) .'</li>';
+						$string.= sprintf('<li><span>%s: %s</span>%s</li>', $k, get_class($v)
+							, self::array2ulli(self::object2array($v, 'definition'===$k?'definition':null), $path.'/'.$k)
+						);
 						break;
 					default:
-						$string.= '<li'.$open.'><span>'.$k.'</span>'. self::array2ulli($v, $path.'/'.$k).'</li>';
+						$string.= sprintf('<li%s><span>%s</span>%s</li>', $open, $k, self::array2ulli($v, $path.'/'.$k));
 						break;
 				}
 			}
@@ -86,38 +112,48 @@ class Tracer
 		return $string.'</ul>';
 	}
 
+	/**
+	 * @param string $name
+	 * @param mixed $data
+	 */
 	public static function add($name, $data=array())
 	{
-		if( $name )
+		if (empty(self::$tracer[$name]))
+			self::$tracer[$name] = array();
+		foreach($data as $key=>$value)
 		{
-			if(empty(self::$tracer[$name]))
-				self::$tracer[$name] = array();
-			foreach($data as $key=>$value)
-			{
-				self::$tracer[$name][$key] = $value;
-			}
+			self::$tracer[$name][$key] = $value;
 		}
-		elseif( self::$tracer )
+	}
+
+	/**
+	 * end tracer and display code coverage
+	 */
+	public static function out()
+	{
+		self::add(self::CODE_COVERAGE, self::get_code_coverage());
+		if( self::$tracer )
 		{
-			self::add(self::CODE_COVERAGE);
 			?>
 			<ul id="code-tracer" class="tracer">
 				<li class="open"><span>Code trace</span>
-					<?= self::array2ulli(self::$tracer) ?>
+					<?php echo self::array2ulli(self::$tracer) ?>
 				</li>
 			</ul>
 			<link rel="stylesheet" href="?mirror=https://raw.github.com/jzaefferer/jquery-treeview/master/jquery.treeview.css" />
-			<?php	exec('grep jquery-1.*min.js . -l', $jquery_hint);
-					$implode_tracer = implode('', self::$tracer[self::CODE_COVERAGE]);
-					foreach($jquery_hint as $needle)
-						if($jquery_exists = strpos($implode_tracer, $needle))
-							break;
-					if (empty($jquery_exists)): ?>
-			<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-			<?php endif; ?>
+		    <script type="text/javascript">window.jQuery || document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"><\/script>')</script>
 			<script type="text/javascript" src="?mirror=https://raw.github.com/carhartl/jquery-cookie/master/jquery.cookie.js"></script>
 			<script type="text/javascript" src="?mirror=https://raw.github.com/jzaefferer/jquery-treeview/master/jquery.treeview.js"></script>
-			<script type="text/javascript">$(document).ready(function(){$("#code-tracer").treeview({collapsed: true,persist: "cookie"});});</script>
+			<script type="text/javascript">
+				$(document).ready(function(){
+					$("#code-tracer").treeview({collapsed: true,persist: "cookie"});
+					$('.tracefile').next().hide();
+					$('.tracefile').click(function(e){
+						$(this).data('tracefile') || $(this).data('tracefile', $(this).next().html());
+						$(this).next().toggle().load('', 'tracefile=' + $(this).data('tracefile'));
+					});
+				});
+			</script>
 			<?php
 		}
 	}
