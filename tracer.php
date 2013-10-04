d45dfdc4bf6d5c9efa88900ad1c6743cfc1a5951
CJ <lim@chernjie.com>
2013-10-04 17:18:05 +0800

    Objectified Tracer;

--- tracer.php
+++ tracer.php
@@ -1,17 +1,32 @@
 <?php
-	if(isset($_GET['tracefile'])) exit( highlight_file($_GET['tracefile'], true) );
 
-	define('CODE_COVERAGE', 'Code Coverage');
-	error_reporting(E_ALL|E_STRICT);
+class Tracer
+{
+	public static $tracer = array();
+	const CODE_COVERAGE = 'CODE_COVERAGE';
 
-
+	public static function init()
+	{
+		if (array_key_exists('tracefile', $_GET)) exit( highlight_file($_GET['tracefile'], true) );
+		if (array_key_exists('mirror', $_GET))
+		{
+			$cache = '.' . DIRECTORY_SEPARATOR . md5($_GET['mirror']) . '.cache';
+			file_exists($cache)
+				|| file_put_contents($cache, file_get_contents($_GET['mirror']));
+			exit( file_get_contents($cache) );
+		}
+		error_reporting(E_ALL|E_STRICT);
+		xdebug_start_code_coverage();
+		self::add('Environment', array('_GET'=>$_GET, '_POST'=>$_POST, '_COOKIE'=>$_COOKIE, '_SERVER'=>$_SERVER));
+	}
+	
 	/**
 	 * Get code coverage with Xdebug
 	 * @link http://www.xdebug.org/docs/code_coverage
 	 *
 	 * @return array filename as key and codes as array values
 	 */
-	function get_code_coverage()
+	public static function get_code_coverage()
 	{
 		$coverage = xdebug_get_code_coverage();
 		foreach($coverage as $file => $line):
@@ -23,20 +38,20 @@
 				$code.= sprintf('%4d: ',$lineNumber).$file_contents[$lineNumber-1];
 			$coverage[$file] = $code;
 		endforeach;
-		if(!get_value('cc_sort', 'integer', 0)) ksort($coverage);
+		if (array_key_exists('cc_sort', $_GET)) ksort($coverage);
 		return $coverage;
 	}
 
-	function object2array($object, $property=null)
+	private static function object2array($object, $property=null)
 	{
 		$array = preg_replace('/\w+::__set_state/', '', var_export($object, true));
 		eval('$array = ' . $array . ';');
 		return is_null($property) ? $array : $array[$property];
 	}
 
-	function array2ulli($arr, $path='')
+	private static function array2ulli($arr, $path='')
 	{
-		$fn = __FUNCTION__;
+		$fn = __METHOD__;
 		$string = '<ul>';
 		if(is_array( $arr))
 			foreach($arr as $k=>$v)
@@ -59,10 +74,10 @@
 						$string.= '<li><span>'.$k.': '.$v.'</span></li>';
 						break;
 					case is_object($v):
-						$string.= '<li><span>'.$k.': '.get_class($v).'</span>'.$fn(object2array($v, 'definition'===$k?'definition':null), $path.'/'.$k) .'</li>';
+						$string.= '<li><span>'.$k.': '.get_class($v).'</span>'. self::array2ulli(object2array($v, 'definition'===$k?'definition':null), $path.'/'.$k) .'</li>';
 						break;
 					default:
-						$string.= '<li'.$open.'><span>'.$k.'</span>'.$fn($v, $path.'/'.$k).'</li>';
+						$string.= '<li'.$open.'><span>'.$k.'</span>'. self::array2ulli($v, $path.'/'.$k).'</li>';
 						break;
 				}
 			}
@@ -71,59 +86,39 @@
 		return $string.'</ul>';
 	}
 
-	function add2tracer($name, $data=array())
+	public static function add($name, $data=array())
 	{
-		global $tracer;
 		if( $name )
 		{
-			if(empty($tracer[$name]))
-				$tracer[$name] = array();
+			if(empty(self::$tracer[$name]))
+				self::$tracer[$name] = array();
 			foreach($data as $key=>$value)
 			{
-				$tracer[$name][$key] = $value;
-				if($name == CODE_COVERAGE)
-				{
-					foreach(array(
-						'Dao'=>'/sites\/dao\/([a-z0-9_-]+)/i',
-						'Module'=>'/sites\/module\/([a-z0-9_-]+)/i',
-					) as $match=>$regexp)
-					{
-						if( preg_match($regexp, $value, $key) )
-						{
-							$tracer[get_controller().' '.get_action()][$match.': '.$key[1]] = $value;
-						}
-					}
-				}
+				self::$tracer[$name][$key] = $value;
 			}
 		}
-		elseif( $tracer )
+		elseif( self::$tracer )
 		{
+			self::add(self::CODE_COVERAGE);
 			?>
 			<ul id="code-tracer" class="tracer">
 				<li class="open"><span>Code trace</span>
-					<?= array2ulli($tracer) ?>
+					<?= self::array2ulli(self::$tracer) ?>
 				</li>
 			</ul>
-			<link rel="stylesheet" href="/sites/resources/css/treeview/jquery.treeview.css" />
-			<?php	exec('ack jquery-1.*min.js sites/ -l', $jquery_hint);
-					$implode_tracer = implode('',$tracer[CODE_COVERAGE]);
+			<link rel="stylesheet" href="?mirror=https://raw.github.com/jzaefferer/jquery-treeview/master/jquery.treeview.css" />
+			<?php	exec('grep jquery-1.*min.js . -l', $jquery_hint);
+					$implode_tracer = implode('', self::$tracer[self::CODE_COVERAGE]);
 					foreach($jquery_hint as $needle)
 						if($jquery_exists = strpos($implode_tracer, $needle))
 							break;
 					if (empty($jquery_exists)): ?>
-			<script type="text/javascript" src="/sites/resources/js/jquery-1.7.1.min.js"></script>
+			<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 			<?php endif; ?>
-			<script type="text/javascript" src="/sites/resources/js/jquery.cookie.min.js"></script>
-			<script type="text/javascript" src="/sites/resources/js/jquery.treeview.min.js"></script>
+			<script type="text/javascript" src="?mirror=https://raw.github.com/carhartl/jquery-cookie/master/jquery.cookie.js"></script>
+			<script type="text/javascript" src="?mirror=https://raw.github.com/jzaefferer/jquery-treeview/master/jquery.treeview.js"></script>
 			<script type="text/javascript">$(document).ready(function(){$("#code-tracer").treeview({collapsed: true,persist: "cookie"});});</script>
 			<?php
 		}
 	}
-	$tracer = array();
-
-	xdebug_start_code_coverage();
-	require_once("./common/utilities.php");
-	fast_require("Controller", get_framework_common_directory()."/controller-tracer.php");
-	add2tracer('Environment', array('_GET'=>$_GET, '_POST'=>$_POST, '_COOKIE'=>$_COOKIE, '_SERVER'=>$_SERVER));
-	require_once("./index.php");
-?>
+}
